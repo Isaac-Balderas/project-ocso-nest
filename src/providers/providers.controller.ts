ab6873992202e153e8a88ed039830d9dde7396fe
Isaac-Balderas <raul.balderas.ga@gmail.com>
2025-03-11 22:58:20 -0600

    Auth decorator implemented

--- src/providers/providers.controller.ts
+++ src/providers/providers.controller.ts
@@ -2,13 +2,10 @@ import { Controller, Get, Post, Body, Patch, Param, Delete, NotFoundException, U
 import { ProvidersService } from './providers.service';
 import { CreateProviderDto } from './dto/create-provider.dto';
 import { UpdateProviderDto } from './dto/update-provider.dto';
-import { AuthGuard } from 'src/auth/guards/auth.guards';
 import { UserData } from 'src/auth/decorators/user.decorator';
 import { User } from 'src/auth/entities/user.entity';
-import { Roles } from 'src/auth/decorators/roles.decorator';
-import { RolesGuard } from 'src/auth/guards/role.guards';
+import { Auth } from 'src/auth/decorators/auth.decorator';
 
-@UseGuards(AuthGuard)
 @Controller('providers')
 export class ProvidersController {
   constructor(private readonly providersService: ProvidersService) {}
@@ -17,9 +14,7 @@ export class ProvidersController {
   create(@Body() createProviderDto: CreateProviderDto) {
     return this.providersService.create(createProviderDto);
   }
-
-  @UseGuards(RolesGuard)
-  @Roles(["Admin"])
+  @Auth("Admin")
   @Get()
   findAll(@UserData() user: User) {
     if (user.userRoles.includes("Employee")) throw new UnauthorizedException("You are not authorized,  only admins and managers");
